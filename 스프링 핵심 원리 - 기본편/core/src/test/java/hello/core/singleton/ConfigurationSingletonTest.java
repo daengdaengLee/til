05a5dd5c0083a6d6c98e67854a286557cb9234ad
daengdaengLee <gunho1020@gmail.com>
2023-08-14 10:25:08 +0900

    feat(스프링 핵심 원리 기본): @Configuration과 바이트코드 조작의 마법

--- 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -30,4 +30,13 @@ public class ConfigurationSingletonTest {
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
 
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        // CGLIB 클래스
+        System.out.println("bean = " + bean.getClass());
+    }
+
 }
