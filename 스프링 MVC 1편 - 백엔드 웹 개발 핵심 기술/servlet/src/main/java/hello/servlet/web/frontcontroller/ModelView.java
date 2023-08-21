f42bc92e8e24dd0e6a75ab53dc27b142c3fb6195
daengdaengLee <gunho1020@gmail.com>
2023-08-21 11:26:30 +0900

    feat(스프링 MVC 1): Model 추가 - v3

---
+++ 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/servlet/src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,19 @@
+package hello.servlet.web.frontcontroller;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class ModelView {
+
+    private final String viewName;
+
+    private final Map<String, Object> model = new HashMap<>();
+
+    public ModelView(String viewName) {
+        this.viewName = viewName;
+    }
+
+}
