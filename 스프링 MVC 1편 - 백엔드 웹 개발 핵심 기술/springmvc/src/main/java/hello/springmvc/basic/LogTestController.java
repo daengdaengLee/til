afdfb8b3fa0a9a3591c7e49eaf4f410cb4e5e5c3
daengdaengLee <gunho1020@gmail.com>
2023-08-22 11:12:30 +0900

    feat(스프링 MVC 1): 로깅 간단히 알아보기

---
+++ 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+        // 이렇게 사용하지 말 것! 로그 레벨에 따라 불필요한 연산 일어나서 리소스 사용함
+        log.trace("trace log=" + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+
+}
