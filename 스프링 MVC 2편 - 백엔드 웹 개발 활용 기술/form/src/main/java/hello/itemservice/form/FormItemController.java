02aaabc9e25b087cb7406719168a40dcc9735cae
daengdaengLee <gunho1020@gmail.com>
2023-08-27 17:47:46 +0900

    feat(스프링 MVC 2): 체크 박스 - 단일1

--- 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/form/src/main/java/hello/itemservice/form/FormItemController.java
+++ 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/form/src/main/java/hello/itemservice/form/FormItemController.java
@@ -3,11 +3,13 @@ package hello.itemservice.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -36,6 +38,8 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item={}", item);
+
         var savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
