eace3368032b4029ea3633c05507dd7be1b755f0
daengdaengLee <gunho1020@gmail.com>
2023-08-29 11:35:40 +0900

    feat(스프링 MVC 2): Validator 분리1

--- 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -20,6 +20,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequiredArgsConstructor
 public class ValidationItemControllerV2 {
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -303,7 +304,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    @PostMapping("/add/v4-2")
     public String addItemV4_2(
             @ModelAttribute Item item,
             BindingResult bindingResult,
@@ -332,6 +333,26 @@ public class ValidationItemControllerV2 {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        var savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(
+            @ModelAttribute Item item,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+        this.itemValidator.validate(item, bindingResult);
 
         // 검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
