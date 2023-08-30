f59f81135e18546c075cab27d87f67f7211023b6
daengdaengLee <gunho1020@gmail.com>
2023-08-30 18:09:45 +0900

    feat(스프링 MVC 2): 로그인 처리하기 - 쿠키 사용

--- 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/login/src/main/java/hello/login/web/login/LoginController.java
+++ 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/login/src/main/java/hello/login/web/login/LoginController.java
@@ -1,6 +1,8 @@
 package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +24,10 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(
+            @Valid @ModelAttribute LoginForm form,
+            BindingResult bindingResult,
+            HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
             return "login/loginForm";
@@ -36,8 +41,23 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // 로그인 성공 처리 TODO
+        // 로그인 성공 처리
+        // 쿠키에 시간 정보를 주지 않으면 세션 쿠키 (브라우저 종료시 모두 종료)
+        var idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        this.expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
