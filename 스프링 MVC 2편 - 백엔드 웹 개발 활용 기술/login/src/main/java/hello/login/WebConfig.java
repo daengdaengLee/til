c0676275361998a199abfb4f5cd463ee335e2304
daengdaengLee <gunho1020@gmail.com>
2023-08-31 10:01:40 +0900

    feat(스프링 MVC 2): 스프링 인터셉터 - 인증 체크

--- 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/login/src/main/java/hello/login/WebConfig.java
+++ 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/login/src/main/java/hello/login/WebConfig.java
@@ -3,9 +3,9 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,9 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
     }
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean<Filter> logFilter() {
         var filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -30,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
         return filterRegistrationBean;
     }
 
-    @Bean
+    //    @Bean
     public FilterRegistrationBean<Filter> loginCheckFilter() {
         var filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
