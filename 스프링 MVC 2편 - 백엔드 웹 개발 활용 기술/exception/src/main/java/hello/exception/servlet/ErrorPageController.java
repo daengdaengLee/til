861e210b98c0f174fa4ac4e7e85ef116c2a11c6f
daengdaengLee <gunho1020@gmail.com>
2023-09-01 10:10:54 +0900

    feat(스프링 MVC 2): 서블릿 예외 처리 - 오류 페이지 작동 원리

--- 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,5 +1,6 @@
 package hello.exception.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -12,12 +13,24 @@ public class ErrorPageController {
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("error page 404");
+        this.printErrorInfo(request);
         return "error-page/404";
     }
 
     @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("error page 500");
+        this.printErrorInfo(request);
         return "error-page/500";
+    }
+
+    private void printErrorInfo(HttpServletRequest request) {
+        log.info("ERROR_EXCEPTION: {}", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}", request.getAttribute(RequestDispatcher.ERROR_MESSAGE));
+        log.info("ERROR_REQUEST_URI: {}", request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(RequestDispatcher.ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
+        log.info("dispatcherType={}", request.getDispatcherType());
     }
 }
