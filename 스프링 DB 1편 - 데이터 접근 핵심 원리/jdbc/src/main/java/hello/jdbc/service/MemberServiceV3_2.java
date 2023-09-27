6efb035b90473fcd5cad1f68cf7902a131921f17
daengdaengLee <gunho1020@gmail.com>
2023-09-27 13:34:24 +0900

    feat(스프링 DB 1): 트랜잭션 문제 해결 - 트랜잭션 템플릿

---
+++ 스프링 DB 1편 - 데이터 접근 핵심 원리/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,49 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) {
+        this.txTemplate.executeWithoutResult((status) -> {
+            // 비즈니스 로직
+            try {
+                this.bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        var fromMember = this.memberRepository.findById(fromId);
+        var toMember = this.memberRepository.findById(toId);
+
+        this.memberRepository.update(fromId, fromMember.getMoney() - money);
+        this.validation(toMember);
+        this.memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
---
+++ 스프링 DB 1편 - 데이터 접근 핵심 원리/jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
@@ -0,0 +1,88 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ */
+@Slf4j
+@SpringBootTest // 로그를 위해
+class MemberServiceV3_2Test {
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV3 memberRepository;
+    private MemberServiceV3_2 memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        var dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        this.memberRepository = new MemberRepositoryV3(dataSource);
+        var transactionManager = new DataSourceTransactionManager(dataSource);
+        this.memberService = new MemberServiceV3_2(transactionManager, this.memberRepository);
+    }
+
+    @AfterEach
+    void afterEach() throws SQLException {
+        this.memberRepository.delete(MEMBER_A);
+        this.memberRepository.delete(MEMBER_B);
+        this.memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        // given
+        var memberA = new Member(MEMBER_A, 10000);
+        var memberB = new Member(MEMBER_B, 10000);
+        this.memberRepository.save(memberA);
+        this.memberRepository.save(memberB);
+
+        // when
+        log.info("START TX");
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        log.info("END TX");
+
+        // then
+        var findMemberA = memberRepository.findById(memberA.getMemberId());
+        var findMemberB = memberRepository.findById(memberB.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        // given
+        var memberA = new Member(MEMBER_A, 10000);
+        var memberEx = new Member(MEMBER_EX, 10000);
+        this.memberRepository.save(memberA);
+        this.memberRepository.save(memberEx);
+
+        // when
+        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        // then
+        var findMemberA = memberRepository.findById(memberA.getMemberId());
+        var findMemberB = memberRepository.findById(memberEx.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        assertThat(findMemberB.getMoney()).isEqualTo(10000);
+    }
+}
