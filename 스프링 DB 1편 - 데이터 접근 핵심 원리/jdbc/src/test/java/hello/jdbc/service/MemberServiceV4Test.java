d778fc83ef8c1f77f2f3b95134f28414a71aba65
daengdaengLee <gunho1020@gmail.com>
2023-09-30 22:04:43 +0900

    feat(스프링 DB 1): 스프링 예외 추상화 적용

--- 스프링 DB 1편 - 데이터 접근 핵심 원리/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ 스프링 DB 1편 - 데이터 접근 핵심 원리/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -2,7 +2,7 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -40,7 +40,8 @@ class MemberServiceV4Test {
     static class TestConfig {
         @Bean
         MemberRepository memberRepository(DataSource dataSource) {
-            return new MemberRepositoryV4_1(dataSource);
+//            return new MemberRepositoryV4_1(dataSource);
+            return new MemberRepositoryV4_2(dataSource);
         }
 
         @Bean
