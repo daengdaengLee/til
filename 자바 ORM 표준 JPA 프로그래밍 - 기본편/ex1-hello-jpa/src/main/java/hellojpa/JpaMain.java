f1b38d3b174e9e516e86fea072e1f797c3ed9675
daengdaengLee <gunho1020@gmail.com>
2023-10-13 08:07:03 +0900

    feat(JPA 기본편): 섹션 5. 연관관계 매핑 기초

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,13 +19,22 @@ public class JpaMain {
 
             var member = new Member();
             member.setName("member1");
+            // 여기는 필수
             member.setTeam(team);
             em.persist(member);
 
+            // 객체지향적으로 생각하면 여기도 추가해줘야 한다.
+            // 순수 객체 상태를 고려해서 항상 양쪽에 값을 설정하자.
             team.getMembers().add(member);
 
-            em.flush();
-            em.clear();
+//            em.flush();
+//            em.clear();
+
+            var findTeam = em.find(Team.class, team.getId()); // 1차 캐시
+            var members = findTeam.getMembers();
+            for (Member m : members) {
+                System.out.println("m = " + m.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
