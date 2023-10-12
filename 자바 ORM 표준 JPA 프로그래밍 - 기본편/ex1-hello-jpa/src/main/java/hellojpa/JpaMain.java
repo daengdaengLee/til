ac57636e7882757379f66882eff7c389846de1d2
daengdaengLee <gunho1020@gmail.com>
2023-10-12 15:34:56 +0900

    feat(JPA 기본편): 섹션 4. 엔티티 매핑

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -12,6 +12,25 @@ public class JpaMain {
         tx.begin();
 
         try {
+            var member1 = new Member();
+            member1.setUsername("A");
+
+            var member2 = new Member();
+            member2.setUsername("B");
+
+            var member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("BEFORE");
+            em.persist(member1); // 1, 51
+            em.persist(member2); // MEM(2)
+            em.persist(member3); // MEM(3)
+
+            System.out.println("member1.id = " + member1.getId());
+            System.out.println("member2.id = " + member2.getId());
+            System.out.println("member3.id = " + member3.getId());
+            System.out.println("AFTER");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,30 +2,37 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1,
+        allocationSize = 50)
 public class Member {
     @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
     @Column(name = "name", nullable = false)
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-    @Lob
-    private String description;
-    @Transient
-    private int temp;
 
     public Member() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
