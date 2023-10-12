669b9c7c364222ad02cabc5f14ea530b6b6588d3
daengdaengLee <gunho1020@gmail.com>
2023-10-12 10:35:02 +0900

    feat(JPA 기본편): 섹션 3. 영속성 관리 - 내부 동작 방식

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
-        new JpaMain().listAll();
+        new JpaMain().detach();
     }
 
     private void create() {
@@ -114,6 +114,211 @@ public class JpaMain {
                 System.out.println("member.id = " + member.getId());
                 System.out.println("member.name = " + member.getName());
             }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private void persistenceContext1() {
+        var emf = Persistence.createEntityManagerFactory("hello");
+
+        var em = emf.createEntityManager();
+
+        var tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 비영속
+            var member = new Member();
+            member.setId(1L);
+            member.setName("HelloJPA");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            // 준영속
+            // em.detach(member);
+
+            // 삭제
+            // em.remove(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private void persistenceContext2() {
+        var emf = Persistence.createEntityManagerFactory("hello");
+
+        var em = emf.createEntityManager();
+
+        var tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 비영속
+            var member = new Member();
+            member.setId(1L);
+            member.setName("HelloJPA");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            var findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private void persistenceContext3() {
+        var emf = Persistence.createEntityManagerFactory("hello");
+
+        var em = emf.createEntityManager();
+
+        var tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 영속
+            var findMember1 = em.find(Member.class, 1L);
+            var findMember2 = em.find(Member.class, 1L);
+            System.out.println("result = " + (findMember1 == findMember2));
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private void persistenceContext4() {
+        var emf = Persistence.createEntityManagerFactory("hello");
+
+        var em = emf.createEntityManager();
+
+        var tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 영속
+            var memberA = new Member(101L, "A");
+            var memberB = new Member(102L, "B");
+
+            em.persist(memberA);
+            em.persist(memberB);
+
+            System.out.println("==========");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private void persistenceContext5() {
+        var emf = Persistence.createEntityManagerFactory("hello");
+
+        var em = emf.createEntityManager();
+
+        var tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 영속
+            var member = em.find(Member.class, 1L);
+            member.setName("zzz");
+
+            // 필요 없음!!! JPA 의 더티 체킹 덕분에!!!
+            // em.persist(member);
+
+            System.out.println("==========");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private void flush() {
+        var emf = Persistence.createEntityManagerFactory("hello");
+
+        var em = emf.createEntityManager();
+
+        var tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 영속
+            var member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush();
+
+            System.out.println("==========");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private void detach() {
+        var emf = Persistence.createEntityManagerFactory("hello");
+
+        var em = emf.createEntityManager();
+
+        var tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 영속
+            var member = em.find(Member.class, 1L);
+            member.setName("aaa");
+
+            // em.detach(member);
+            em.clear();
+
+            em.find(Member.class, 1L);
+
+            System.out.println("==========");
 
             tx.commit();
         } catch (Exception e) {
