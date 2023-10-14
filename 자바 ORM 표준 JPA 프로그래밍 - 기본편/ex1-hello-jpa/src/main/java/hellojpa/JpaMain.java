723a38749ed2dd327a0a4b8699dabddc90efd56e
daengdaengLee <gunho1020@gmail.com>
2023-10-14 15:17:20 +0900

    feat(JPA 기본편): 섹션 7. 고급 매핑
    상속관계 매핑

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -12,7 +12,18 @@ public class JpaMain {
         tx.begin();
 
         try {
+            var movie = new Movie();
+            movie.setDirector("A");
+            movie.setActor("B");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10_000);
 
+            em.persist(movie);
+
+            em.flush();
+            em.clear();
+
+            em.find(Movie.class, movie.getId());
 
             tx.commit();
         } catch (Exception e) {
