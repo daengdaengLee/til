1c2d39f36e92786336a1d56a3593c98761fbc532
daengdaengLee <gunho1020@gmail.com>
2023-10-15 16:18:42 +0900

    feat(우아한 Rust): 섹션 5. Enum과 패턴매칭
    Enum과 패턴매칭

--- 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/enum_part2/src/main.rs
+++ 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/enum_part2/src/main.rs
@@ -1,22 +1,20 @@
 fn main() {
-    let red = color_to_rgb(Color::Red);
-    println!("red rgb = {:?}", red);
+    handle_message(&Message::StartGame);
+    handle_message(&Message::WinPoint { who: String::from("홍길동") });
+    handle_message(&Message::ChangePlayerName(String::from("둘리")));
 }
 
 #[derive(Debug)]
-enum Color {
-    Red,
-    Green,
-    Blue,
+enum Message {
+    StartGame,
+    WinPoint { who: String },
+    ChangePlayerName(String),
 }
 
-#[derive(Debug)]
-struct RGB(u8, u8, u8);
-
-fn color_to_rgb(color: Color) -> RGB {
-    match color {
-        Color::Red => RGB(255, 0, 0),
-        Color::Green => RGB(0, 255, 0),
-        Color::Blue => RGB(0, 0, 255),
-    }
+fn handle_message(message: &Message) {
+    match message {
+        Message::StartGame => println!("게임시작!"),
+        Message::WinPoint { who } => println!("{}의 득점", who),
+        Message::ChangePlayerName(name) => println!("플레이어 이름 변경 => {}", name),
+    };
 }
