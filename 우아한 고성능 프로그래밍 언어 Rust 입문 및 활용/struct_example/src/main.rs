5f71b6d6341ad940698a8c29d9081b92c1b0e560
daengdaengLee <gunho1020@gmail.com>
2023-10-15 14:45:45 +0900

    feat(우아한 Rust): 섹션 4. Struct
    구조체 예제

--- 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/struct_example/src/main.rs
+++ 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/struct_example/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let width = 20;
-    let height = 30;
+    let rect = (20, 30);
 
-    println!("해당 사각형의 면적은 {}.", area(width, height));
+    println!("해당 사각형의 면적은 {}.", area(rect));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rect: (u32, u32)) -> u32 {
+    rect.0 * rect.1
 }
