94c1c1ad37055af5709717d17d1b395136882231
daengdaengLee <gunho1020@gmail.com>
2023-10-15 14:33:47 +0900

    feat(우아한 Rust): 섹션 4. Struct
    구조체 기초

--- 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/struct_chapter/src/main.rs
+++ 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/struct_chapter/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    let user = User {
+    let mut user = User {
         name: String::from("홍길동"),
         email: String::from("gildong@example.com"),
         active: true,
     };
 
-    println!("이용자의 이름은 = {}", user.name);
+    user.email = String::from("gd.hong@example.com");
+
+    println!("이용자의 이메일 = {}", user.email);
 }
 
 struct User {
