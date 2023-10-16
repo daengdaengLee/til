fe85275726559708d7912cd172efa1f78aaeadd5
daengdaengLee <gunho1020@gmail.com>
2023-10-16 16:34:37 +0900

    feat(우아한 Rust): 섹션 6. 탄탄한 에러 처리
    에러를 전파하는 방법

--- 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/error_handle/src/main.rs
+++ 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/error_handle/src/main.rs
@@ -2,13 +2,12 @@ use std::io::{Error, Read};
 use std::fs::File;
 
 fn main() {
-    let username = read_username().unwrap();
-    println!("username = {}", username);
+    let username = read_username();
+    println!("username = {:?}", username);
 }
 
 fn read_username() -> Result<String, Error> {
-    let mut file = File::open("hello.txt")?;
     let mut username = String::new();
-    file.read_to_string(&mut username)?;
+    File::open("hello.txt")?.read_to_string(&mut username)?;
     Ok(username)
 }
