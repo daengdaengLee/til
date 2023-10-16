6c3ad00c7c985fc8507bc2d2336d2fe7f422345a
daengdaengLee <gunho1020@gmail.com>
2023-10-16 14:49:31 +0900

    feat(JPA 기본편): 섹션 9. 값 타입
    값 타입과 불변 객체

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,12 +14,24 @@ public class JpaMain {
         tx.begin();
 
         try {
-            var member = new Member();
-            member.setName("user1");
-            member.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
-            member.setHomeAddress(new Address("city", "street", "zipcode"));
+            var address = new Address("city", "street", "zipcode");
 
-            em.persist(member);
+            var member1 = new Member();
+            member1.setName("member1");
+            member1.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
+            member1.setHomeAddress(address);
+            em.persist(member1);
+
+            var member2 = new Member();
+            member2.setName("member2");
+            member2.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
+            member2.setHomeAddress(address);
+            em.persist(member2);
+
+            // ...
+
+            // !!! member1, member2 모두 newCity 로 바뀜 !!!
+            member1.getHomeAddress().setCity("newCity");
 
             tx.commit();
         } catch (Exception e) {
