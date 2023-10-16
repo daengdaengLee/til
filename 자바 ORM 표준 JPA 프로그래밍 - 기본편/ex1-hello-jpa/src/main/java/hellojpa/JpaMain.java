3b39cfd0262850001f9474a5746bea298f89bab6
daengdaengLee <gunho1020@gmail.com>
2023-10-16 15:35:33 +0900

    feat(JPA 기본편): 섹션 9. 값 타입
    값 타입 컬렉션

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,8 +2,6 @@ package hellojpa;
 
 import jakarta.persistence.Persistence;
 
-import java.time.LocalDateTime;
-
 public class JpaMain {
     public static void main(String[] args) {
         var emf = Persistence.createEntityManagerFactory("hello");
@@ -14,28 +12,49 @@ public class JpaMain {
         tx.begin();
 
         try {
-            var address = new Address("city", "street", "zipcode");
+            var member = new Member();
+            member.setName("member1");
+            member.setHomeAddress(new Address("homeCity", "street1", "10000"));
 
-            var member1 = new Member();
-            member1.setName("member1");
-            member1.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
-            member1.setHomeAddress(address);
-            em.persist(member1);
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
 
-            var member2 = new Member();
-            member2.setName("member2");
-            member2.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
-            member2.setHomeAddress(address);
-            em.persist(member2);
+            member.getAddressHistory().add(new Address("old1", "street1", "10000"));
+            member.getAddressHistory().add(new Address("old2", "street1", "10000"));
 
-            // ...
+            em.persist(member);
 
-            // BEST
-            // 불변 객체를 통해 부작용을 원천 차단
-            member1.setHomeAddress(new Address(
-                    "newCity",
-                    member1.getHomeAddress().getStreet(),
-                    member1.getHomeAddress().getZipcode()));
+            em.flush();
+            em.clear();
+
+            System.out.println("==========");
+
+            var findMember = em.find(Member.class, member.getId());
+            var addressHistory = findMember.getAddressHistory();
+            for (var address : addressHistory) {
+                System.out.println("address.city = " + address.getCity());
+            }
+            var favoriteFoods = findMember.getFavoriteFoods();
+            for (var favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFood = " + favoriteFood);
+            }
+
+            System.out.println("==========");
+
+            // homeCity -> newCity
+            var homeAddress = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", homeAddress.getStreet(), homeAddress.getZipcode()));
+
+            // 치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            // 쿼리 잘 확인
+            // 전부 다 지우고 현재 상태로 다시 insert 함
+            // 실무에서는 값 타입 컬렉션 대신 1:N 관계를 고려
+            findMember.getAddressHistory().remove(new Address("old1", "street1", "10000"));
+            findMember.getAddressHistory().add(new Address("new1", "street1", "10000"));
 
             tx.commit();
         } catch (Exception e) {
--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,7 +3,9 @@ package hellojpa;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member extends BaseEntity {
@@ -23,6 +25,17 @@ public class Member extends BaseEntity {
     private Period workPeriod;
     @Embedded
     private Address homeAddress;
+    @ElementCollection(fetch = FetchType.LAZY) // LAZY 가 기본값
+    @CollectionTable(
+            name = "FAVORITE_FOOD",
+            joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+    @ElementCollection
+    @CollectionTable(
+            name = "ADDRESS",
+            joinColumns = @JoinColumn(name = "MEMBER_ID "))
+    private List<Address> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -70,5 +83,21 @@ public class Member extends BaseEntity {
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
