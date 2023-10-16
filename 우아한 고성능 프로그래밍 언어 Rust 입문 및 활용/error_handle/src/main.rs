2ec6fb677338ce9da3d93adaa46d54b4facb116e
daengdaengLee <gunho1020@gmail.com>
2023-10-16 16:26:35 +0900

    feat(우아한 Rust): 섹션 6. 탄탄한 에러 처리
    복구 가능한 에러와 불가능한 에러 구분

--- 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/error_handle/src/main.rs
+++ 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/error_handle/src/main.rs
@@ -1,15 +1,7 @@
 use std::fs::File;
-use std::io::ErrorKind;
 
 fn main() {
-    let file = File::open("hello.txt");
-    let file = match file {
-        Ok(f) => Ok(f),
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => File::create("hello.txt"),
-            _ => panic!("파일 접근 실패"),
-        },
-    };
+    let file: File = File::open("hello.txt").unwrap();
 }
 
 // 기본 내장
