a3af2036c973b05b1e9362f8c8fa58115d6450f7
daengdaengLee <gunho1020@gmail.com>
2023-10-17 15:46:47 +0900

    feat(JPA 기본편): 섹션 10. 객체지향 쿼리 언어1 - 기본 문법
    프로젝션(SELECT)

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/jpql/src/main/java/hellojpa/Main.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/jpql/src/main/java/hellojpa/Main.java
@@ -1,7 +1,7 @@
 package hellojpa;
 
+import hellojpa.jpql.Address;
 import hellojpa.jpql.Member;
-import hellojpa.jpql.Team;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
 
@@ -27,11 +27,7 @@ public class Main {
             em.flush();
             em.clear();
 
-            // 실무에서는 아래처럼 실제 SQL과 비슷한 모양으로 맞춰서 쓸 것 -> join 한다! 알아볼 수 있게
-            // join 쿼리는 성능 예측하기 어렵기 때문
-            em.createQuery("select m.team from Member as m", Team.class)
-                    .getResultList();
-            em.createQuery("select t from Member as m join m.team as t", Team.class)
+            em.createQuery("select o.address from Order as o", Address.class)
                     .getResultList();
 
             tx.commit();
