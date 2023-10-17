2c1adff0e98c9d73860a8dbec33e358394949e84
daengdaengLee <gunho1020@gmail.com>
2023-10-17 10:44:49 +0900

    feat(우아한 Rust): 섹션 7. 제네릭과 트레이트, 그리고 임대값의 수명
    제네릭 | 공통 코드로 다양한 타입에 대해 처리하자

--- 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/generics_example/src/main.rs
+++ 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/generics_example/src/main.rs
@@ -1,14 +1,14 @@
-fn main() {
-    let p1: Point<i32> = Point { x: 2, y: 3 };
-    let p2: Point<f64> = Point { x: 5.0, y: 1.0 };
-    // x, y 는 같은 타입으로 선언했기 때문에 불가능
-    // let p3 = Point { x: 2, y: 3.0 };
-    println!("p1 = {:?}, p2 = {:?}", p1, p2);
+fn main() {}
+
+// enum 에 제네릭 선언
+// 기본으로 선언되어 있음
+
+enum Option<T> {
+    Some(T),
+    None,
 }
 
-// struct 에 제네릭 선언
-#[derive(Debug)]
-struct Point<T> {
-    x: T,
-    y: T,
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
 }
