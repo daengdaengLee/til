db73f874cfe5d17c69b66c66f5e1ee9886fd86c2
daengdaengLee <gunho1020@gmail.com>
2023-10-17 14:18:06 +0900

    feat(JPA 기본편): 섹션 10. 객체지향 쿼리 언어1 - 기본 문법
    소개

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -12,52 +12,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            var member = new Member();
-            member.setName("member1");
-            member.setHomeAddress(new Address("homeCity", "street1", "10000"));
+            var result = em.createQuery("select m from Member m where m.name like '%kim%'", Member.class)
+                    .getResultList();
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street1", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street1", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("==========");
-
-            var findMember = em.find(Member.class, member.getId());
-            var addressHistory = findMember.getAddressHistory();
-            for (var address : addressHistory) {
-                System.out.println("address.city = " + address.getAddress().getCity());
+            for (var member : result) {
+                System.out.println("member = " + member);
             }
-            var favoriteFoods = findMember.getFavoriteFoods();
-            for (var favoriteFood : favoriteFoods) {
-                System.out.println("favoriteFood = " + favoriteFood);
-            }
-
-            System.out.println("==========");
-
-            // homeCity -> newCity
-            var homeAddress = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", homeAddress.getStreet(), homeAddress.getZipcode()));
-
-            // 치킨 -> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            // 쿼리 잘 확인
-            // 전부 다 지우고 현재 상태로 다시 insert 함
-            // 실무에서는 값 타입 컬렉션 대신 1:N 관계를 고려
-            // findMember.getAddressHistory().remove(new Address("old1", "street1", "10000"));
-            // findMember.getAddressHistory().add(new Address("new1", "street1", "10000"));
-            var oldAddressHistory = findMember.getAddressHistory().get(0).getAddress();
-            findMember.getAddressHistory().get(0).setAddress(
-                    new Address("new1", oldAddressHistory.getStreet(), oldAddressHistory.getZipcode()));
 
             tx.commit();
         } catch (Exception e) {
