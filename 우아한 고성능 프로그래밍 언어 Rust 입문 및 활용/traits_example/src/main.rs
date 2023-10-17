33a0ecc0dd1bca70aae5e6abe60fda98598404bf
daengdaengLee <gunho1020@gmail.com>
2023-10-17 10:56:31 +0900

    feat(우아한 Rust): 섹션 7. 제네릭과 트레이트, 그리고 임대값의 수명
    트레이트 | 제너릭 타입의 범위를 한정하기

--- 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/traits_example/src/main.rs
+++ 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/traits_example/src/main.rs
@@ -4,9 +4,13 @@ fn main() {
         name: String::from("홍길동"),
         active: true,
     };
-    meet(&cat, &gildong);
-    println!("==========");
-    meet(&gildong, &cat);
+    // 트레잇 바운드로 같은 타입으로 한정했기 때문에 Pet, Person 끼리 인사 못 함
+    // meet(&cat, &gildong);
+    // println!("==========");
+    // meet(&gildong, &cat);
+
+    // Greet 구현하고 있는 "같은" 타입끼리만 인사 가능
+    meet(&cat, &Pet::Dog);
 }
 
 trait Greet {
@@ -40,7 +44,7 @@ impl Greet for Person {
     }
 }
 
-fn meet(one: &impl Greet, another: &impl Greet) {
+fn meet<T: Greet>(one: &T, another: &T) {
     println!("첫번째가 인사합니다 = {}", one.greeting());
     println!("두번째가 인사합니다 = {}", another.greeting());
 }
