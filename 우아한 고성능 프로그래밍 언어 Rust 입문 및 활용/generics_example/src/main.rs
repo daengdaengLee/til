f1dd9e77646685d173b53113253d7f2014e259c3
daengdaengLee <gunho1020@gmail.com>
2023-10-17 10:43:32 +0900

    feat(우아한 Rust): 섹션 7. 제네릭과 트레이트, 그리고 임대값의 수명
    제네릭 | 공통 코드로 다양한 타입에 대해 처리하자

--- 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/generics_example/src/main.rs
+++ 우아한 고성능 프로그래밍 언어 Rust 입문 및 활용/generics_example/src/main.rs
@@ -1,60 +1,14 @@
 fn main() {
-    let numbers = vec![3, 4, 1, 6, 8, 10];
-    let chars = vec!['홍', '길', '동'];
-
-    let result = smallest_i32(&numbers);
-    println!("가장 작은 수는 {}", result);
-
-    let result = smallest_char(&chars);
-    println!("가장 작은(?) 글자는 {}", result);
-
-    println!("==========");
-
-    let result = smallest(&numbers);
-    println!("가장 작은 수는 {}", result);
-
-    let result = smallest(&chars);
-    println!("가장 작은(?) 글자는 {}", result);
-
-    println!("==========");
-
-    let result = smallest(&["홍길동", "둘리", "도우너"]);
-    println!("가장 작은(?) 이름은 {}", result);
+    let p1: Point<i32> = Point { x: 2, y: 3 };
+    let p2: Point<f64> = Point { x: 5.0, y: 1.0 };
+    // x, y 는 같은 타입으로 선언했기 때문에 불가능
+    // let p3 = Point { x: 2, y: 3.0 };
+    println!("p1 = {:?}, p2 = {:?}", p1, p2);
 }
 
-fn smallest_i32(list: &[i32]) -> &i32 {
-    let mut smallest = &list[0];
-
-    for item in list {
-        if item < smallest {
-            smallest = item;
-        }
-    }
-
-    smallest
-}
-
-fn smallest_char(list: &[char]) -> &char {
-    let mut smallest = &list[0];
-
-    for item in list {
-        if item < smallest {
-            smallest = item;
-        }
-    }
-
-    smallest
-}
-
-// 제네릭 함수로 통합
-fn smallest<T: PartialOrd>(list: &[T]) -> &T {
-    let mut smallest = &list[0];
-
-    for item in list {
-        if item < smallest {
-            smallest = item;
-        }
-    }
-
-    smallest
+// struct 에 제네릭 선언
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
 }
