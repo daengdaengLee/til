78d65903f1729a9aba92357f7a818d92cc670f3d
daengdaengLee <gunho1020@gmail.com>
2023-10-19 22:03:38 +0900

    feat(실전 JPA 1): 섹션 2. 도메인 분석 설계
    - 엔티티 클래스 개발1
    - 엔티티 클래스 개발2

---
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Embeddable
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+}
