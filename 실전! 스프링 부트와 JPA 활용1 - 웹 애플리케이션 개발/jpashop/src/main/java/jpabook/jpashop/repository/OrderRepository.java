ab8aa3ded3a069b06b65e94be331170d0c3dba49
daengdaengLee <gunho1020@gmail.com>
2023-11-06 14:54:07 +0900

    feat(실전 JPA 1): 섹션 6. 주문 도메인 개발
    - 주문 검색 기능 개발

--- 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,9 +1,15 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
@@ -18,6 +24,30 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//    }
+    // @TODO QueryDSL 로 동적 쿼리 처리
+    public List<Order> findAll(OrderSearch orderSearch) {
+        var cb = em.getCriteriaBuilder();
+        var cq = cb.createQuery(Order.class);
+        var o = cq.from(Order.class);
+        var m = o.join("member", JoinType.INNER); // 회원과 조인
+
+        var criteriaList = new ArrayList<Predicate>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            var status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteriaList.add(status);
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            var name = cb.like(m.get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteriaList.add(name);
+        }
+
+        cq.where(cb.and(criteriaList.toArray(new Predicate[criteriaList.size()])));
+        return em.createQuery(cq)
+                .setMaxResults(1000) // 최대 1000건
+                .getResultList();
+    }
 }
