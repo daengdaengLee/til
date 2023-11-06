e7da79c8e54298eaa2c88176086676f88452ce4b
daengdaengLee <gunho1020@gmail.com>
2023-11-06 13:33:51 +0900

    feat(실전 JPA 1): 섹션 6. 주문 도메인 개발
    - 주문, 주문상품 엔티티 개발

--- 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -32,7 +32,8 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    // 연관 관계 편의 메서드
+    // === 연관 관계 편의 메서드 === //
+
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
@@ -46,5 +47,45 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // === 생성 메서드 === //
+
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        var order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (var orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // === 비즈니스 로직 === //
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (getDelivery().getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (var orderItem : getOrderItems()) {
+            orderItem.cancel();
+        }
+    }
+
+    // === 조회 로직 === //
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderItems().stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
     }
 }
