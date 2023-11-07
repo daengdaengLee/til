897c90e42a2bd8756fcd6f1dc0e184f6d3c508ab
daengdaengLee <gunho1020@gmail.com>
2023-11-07 15:28:29 +0900

    feat(실전 JPA 1): 섹션 7. 웹 계층 개발
    - 상품 주문

---
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequiredArgsConstructor
+@Controller
+public class OrderController {
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        var members = memberService.findMembers();
+        var items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
