9fe592889010d325749fc5810b2e4bbc5ff1e80e
daengdaengLee <gunho1020@gmail.com>
2023-11-11 00:14:23 +0900

    feat(실전 JPA 2): 섹션 6. 다음으로
    - QueryDSL 소개

--- 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,20 +1,30 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
 import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
+import jpabook.jpashop.domain.OrderStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RequiredArgsConstructor
+import static jpabook.jpashop.domain.QMember.member;
+import static jpabook.jpashop.domain.QOrder.order;
+
 @Repository
 public class OrderRepository {
     private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
 
     public void save(Order order) {
         em.persist(order);
@@ -51,6 +61,16 @@ public class OrderRepository {
                 .getResultList();
     }
 
+    public List<Order> findAll2(OrderSearch orderSearch) {
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
     public List<Order> findAllWithMemberDelivery() {
         return em.createQuery(
                         "select o from Order o join fetch o.member m join fetch o.delivery d",
@@ -76,5 +96,19 @@ public class OrderRepository {
                         join fetch oi.item i
                         """, Order.class)
                 .getResultList();
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return order.status.eq(statusCond);
+    }
+
+    private BooleanExpression nameLike(String nameCond) {
+        if (!StringUtils.hasText(nameCond)) {
+            return null;
+        }
+        return member.name.like(nameCond);
     }
 }
--- 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/jpashop/src/main/java/jpabook/jpashop/service/springdatajpa/MemberService2.java
+++ 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/jpashop/src/main/java/jpabook/jpashop/service/springdatajpa/MemberService2.java
@@ -1,7 +1,7 @@
 package jpabook.jpashop.service.springdatajpa;
 
 import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.repository.springdatajpa.MemberRepository;
+import jpabook.jpashop.repository.springdatajpa.MemberRepository2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,8 +11,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
-public class MemberService {
-    private final MemberRepository memberRepository;
+public class MemberService2 {
+    private final MemberRepository2 memberRepository2;
 
     /**
      * 회원 가입
@@ -20,7 +20,7 @@ public class MemberService {
     @Transactional
     public Long join(Member member) {
         this.validateDuplicateMember(member);
-        this.memberRepository.save(member);
+        this.memberRepository2.save(member);
         return member.getId();
     }
 
@@ -28,23 +28,23 @@ public class MemberService {
      * 회원 전체 조회
      */
     public List<Member> findMembers() {
-        return this.memberRepository.findAll();
+        return this.memberRepository2.findAll();
     }
 
     /**
      * 회원 단건 조회
      */
     public Member findOne(Long memberId) {
-        return this.memberRepository.findById(memberId).orElse(null);
+        return this.memberRepository2.findById(memberId).orElse(null);
     }
 
     @Transactional
     public void update(Long id, String name) {
-        memberRepository.findById(id).ifPresent(member -> member.setName(name));
+        memberRepository2.findById(id).ifPresent(member -> member.setName(name));
     }
 
     private void validateDuplicateMember(Member member) {
-        var findMembers = this.memberRepository.findByName(member.getName());
+        var findMembers = this.memberRepository2.findByName(member.getName());
         if (!findMembers.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
