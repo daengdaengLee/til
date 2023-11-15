fdc847ab5f4e4c51cce7ebb2f9d7b3cf72f70335
daengdaengLee <gunho1020@gmail.com>
2023-11-15 18:52:54 +0900

    feat(Learn Bevy 0.10): EP7 - Bevy Project Organization and Refactor, Rust Modules, and Bevy Plugins (Step 2)
    - https://www.youtube.com/watch?v=gy2G63SA-W8&list=PLVnntJRoP85JHGX7rGDu6LaF3fmDDbqyd&index=7

--- game/bevy-ball-game/src/main.rs
+++ game/bevy-ball-game/src/main.rs
@@ -1,45 +1,27 @@
-pub mod components;
 pub mod events;
-pub mod resources;
 mod systems;
 
+pub mod enemy;
+mod player;
+pub mod score;
+pub mod star;
+
 use bevy::prelude::*;
+
 use events::*;
-use resources::*;
 use systems::*;
+
+use enemy::EnemyPlugin;
+use player::PlayerPlugin;
+use score::ScorePlugin;
+use star::StarPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .init_resource::<Score>()
-        .init_resource::<HighScores>()
-        .init_resource::<StarSpawnTimer>()
-        .init_resource::<EnemySpawnTimer>()
         .add_event::<GameOver>()
-        .add_systems(
-            Startup,
-            (spawn_camera, spawn_player, spawn_enemies, spawn_stars),
-        )
-        .add_systems(
-            Update,
-            (
-                player_movement,
-                confine_player_movement,
-                enemy_movement,
-                update_enemy_direction,
-                confine_enemy_movement,
-                enemy_hit_player,
-                player_hit_star,
-                update_score,
-                tick_star_spawn_timer,
-                spawn_stars_over_time,
-                tick_enemy_spawn_timer,
-                spawn_enemies_over_time,
-                exit_game,
-                handle_game_over,
-                update_high_scores,
-                high_score_updated,
-            ),
-        )
+        .add_plugins((EnemyPlugin, PlayerPlugin, ScorePlugin, StarPlugin))
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Update, (exit_game, handle_game_over))
         .run();
 }
