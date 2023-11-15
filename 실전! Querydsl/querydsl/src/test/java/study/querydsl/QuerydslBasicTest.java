c9c66b742a7eeb4917df609d9d1630127db103cd
daengdaengLee <gunho1020@gmail.com>
2023-11-15 16:38:07 +0900

    feat(Querydsl): 섹션 3. 기본 문법
    - 상수, 문자 더하기

--- 실전! Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ 실전! Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -462,6 +463,32 @@ public class QuerydslBasicTest {
                         .when(member.age.between(21, 30)).then("21살~30살")
                         .otherwise("기타"))
                 .from(member)
+                .fetch();
+
+        for (var s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    void constant() {
+        var result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (var tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    void concat() {
+        // {username}_{age}
+        var result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetch();
 
         for (var s : result) {
