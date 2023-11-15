67c2fbf266fb70e31e4a42b6a6fc3b9b9405571e
daengdaengLee <gunho1020@gmail.com>
2023-11-15 15:22:45 +0900

    feat(Querydsl): 섹션 3. 기본 문법
    - 정렬

--- 실전! Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ 실전! Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -139,4 +139,37 @@ public class QuerydslBasicTest {
                 .fetchCount();
         System.out.println("fetchCount = " + fetchCount);
     }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순 (desc)
+     * 2. 회원 이름 오름차순 (asc)
+     * 단, 2 에서 회원 이름이 없으면 마지막에 출력 (nulls last)
+     */
+    @Test
+    void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        var members = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        var member5 = members.get(0);
+        var member6 = members.get(1);
+        var memberNull = members.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
+        System.out.println("[TEST_OUTPUT]");
+        for (var member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team = " + member.getTeam());
+        }
+    }
 }
