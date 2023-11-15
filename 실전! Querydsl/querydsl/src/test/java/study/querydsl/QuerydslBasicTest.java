0a33fb28eb8422cc3b700b5e7ce4f0ad78fdae9a
daengdaengLee <gunho1020@gmail.com>
2023-11-15 16:34:32 +0900

    feat(Querydsl): 섹션 3. 기본 문법
    - Case 문

--- 실전! Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ 실전! Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -435,6 +436,36 @@ public class QuerydslBasicTest {
 
         for (var tuple : result) {
             System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    void basicCase() {
+        var result = queryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (var s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    void complexCase() {
+        var result = queryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21살~30살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (var s : result) {
+            System.out.println("s = " + s);
         }
     }
 }
