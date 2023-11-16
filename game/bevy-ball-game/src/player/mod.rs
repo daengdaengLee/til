e049903c755795bbfacb0e0f6ceb337bf3c5f4af
daengdaengLee <gunho1020@gmail.com>
2023-11-16 11:32:10 +0900

    feat(Learn Bevy 0.10): EP8 - Explicit System Ordering and Bevy System Sets (Step 1)
    - https://www.youtube.com/watch?v=i-Wczghlmxc&list=PLVnntJRoP85JHGX7rGDu6LaF3fmDDbqyd&index=8

--- game/bevy-ball-game/src/player/mod.rs
+++ game/bevy-ball-game/src/player/mod.rs
@@ -9,14 +9,8 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player).add_systems(
-            Update,
-            (
-                player_movement,
-                confine_player_movement,
-                enemy_hit_player,
-                player_hit_star,
-            ),
-        );
+        app.add_systems(Startup, spawn_player)
+            .add_systems(Update, (player_movement, confine_player_movement).chain())
+            .add_systems(Update, (enemy_hit_player, player_hit_star));
     }
 }
