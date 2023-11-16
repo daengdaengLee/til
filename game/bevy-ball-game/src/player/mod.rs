eaa0d3f2c7ca4b29aeda2d6ce852b52a3e9cdeef
daengdaengLee <gunho1020@gmail.com>
2023-11-16 11:39:24 +0900

    feat(Learn Bevy 0.10): EP8 - Explicit System Ordering and Bevy System Sets (Step 2)
    - https://www.youtube.com/watch?v=i-Wczghlmxc&list=PLVnntJRoP85JHGX7rGDu6LaF3fmDDbqyd&index=8

--- game/bevy-ball-game/src/player/mod.rs
+++ game/bevy-ball-game/src/player/mod.rs
@@ -5,12 +5,35 @@ use bevy::prelude::*;
 
 use systems::*;
 
+// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+// pub struct MovementSystemSet;
+
+// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+// pub struct ConfinementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub enum PlayerSystemSet {
+    Movement,
+    Confinement,
+}
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player)
-            .add_systems(Update, (player_movement, confine_player_movement).chain())
-            .add_systems(Update, (enemy_hit_player, player_hit_star));
+        app.configure_sets(
+            Update,
+            PlayerSystemSet::Movement.before(PlayerSystemSet::Confinement),
+        )
+        .add_systems(Startup, spawn_player)
+        .add_systems(
+            Update,
+            (
+                player_movement.in_set(PlayerSystemSet::Movement),
+                confine_player_movement.in_set(PlayerSystemSet::Confinement),
+                enemy_hit_player,
+                player_hit_star,
+            ),
+        );
     }
 }
