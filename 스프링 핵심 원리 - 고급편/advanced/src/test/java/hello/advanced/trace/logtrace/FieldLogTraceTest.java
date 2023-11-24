11d9ab7fa2cfbffb0f252f0064fcdd4000592286
daengdaengLee <gunho1020@gmail.com>
2023-11-24 13:09:41 +0900

    feat(스프링 핵심 원리 고급): 섹션 2. 쓰레드 로컬 - ThreadLocal
    - 필드 동기화 - 개발

---
+++ 스프링 핵심 원리 - 고급편/advanced/src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,23 @@
+package hello.advanced.trace.logtrace;
+
+import org.junit.jupiter.api.Test;
+
+class FieldLogTraceTest {
+    FieldLogTrace trace = new FieldLogTrace();
+
+    @Test
+    void begin_end_level2() {
+        var status1 = trace.begin("hello1");
+        var status2 = trace.begin("hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_level2() {
+        var status1 = trace.begin("hello1");
+        var status2 = trace.begin("hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+}
