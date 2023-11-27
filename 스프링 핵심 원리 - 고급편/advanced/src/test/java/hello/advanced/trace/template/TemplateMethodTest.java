5f283f6821bb84ca3f0ed13a0c69b3dca7ea09e9
daengdaengLee <gunho1020@gmail.com>
2023-11-27 14:30:44 +0900

    feat(스프링 핵심 원리 고급): 섹션 3. 템플릿 메서드 패턴과 콜백 패턴
    - 템플릿 메서드 패턴 - 예제2

--- 스프링 핵심 원리 - 고급편/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ 스프링 핵심 원리 - 고급편/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -1,5 +1,8 @@
 package hello.advanced.trace.template;
 
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +32,19 @@ public class TemplateMethodTest {
         var endTime = System.currentTimeMillis();
         var resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        // 다형성 사용 (타입: AbstractTemplate, 실제 값: SubClassLogic1)
+
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
     }
 }
