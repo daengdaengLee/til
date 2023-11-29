1652b6d41bb94f3fcad4d52e33a80d528d5dba61
daengdaengLee <gunho1020@gmail.com>
2023-11-29 23:48:44 +0900

    feat(Learn Bevy 0.10): EP10 - Bevy UI (User Interface Tutorial) Part 4
    - https://www.youtube.com/watch?v=iW19V3a96tY&list=PLVnntJRoP85JHGX7rGDu6LaF3fmDDbqyd&index=10

--- game/bevy-ball-game/src/main_menu/styles.rs
+++ game/bevy-ball-game/src/main_menu/styles.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 
 pub const NORMAL_BUTTON_COLOR: Color = Color::rgb(0.15, 0.15, 0.15);
+pub const HOVERED_BUTTON_COLOR: Color = Color::rgb(0.25, 0.25, 0.25);
+pub const PRESSED_BUTTON_COLOR: Color = Color::rgb(0.35, 0.35, 0.35);
 
 pub const MAIN_MENU_STYLE: Style = {
     let mut style = Style::DEFAULT;
