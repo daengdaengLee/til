0821ed02398941a60b5a7bef980f78070be9dac3
daengdaengLee <gunho1020@gmail.com>
2023-11-29 12:52:16 +0900

    feat(스프링 핵심 원리 고급): 섹션 4. 프록시 패턴과 데코레이터 패턴
    - 프록시 패턴 - 예제 코드2

--- 스프링 핵심 원리 - 고급편/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ 스프링 핵심 원리 - 고급편/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,16 @@ public class ProxyPatternTest {
     void noProxyTest() {
         var subject = new RealSubject();
         var client = new ProxyPatternClient(subject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        var subject = new RealSubject();
+        var proxy = new CacheProxy(subject);
+        var client = new ProxyPatternClient(proxy);
         client.execute();
         client.execute();
         client.execute();
