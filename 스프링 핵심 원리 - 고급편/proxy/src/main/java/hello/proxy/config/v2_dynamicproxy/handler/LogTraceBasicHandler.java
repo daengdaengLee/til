76be2cac855577c7e481eaeba67185cecaf75631
daengdaengLee <gunho1020@gmail.com>
2023-11-30 11:59:04 +0900

    feat(스프링 핵심 원리 고급): 섹션 5. 동적 프록시 기술
    - JDK 동적 프록시 - 적용1

---
+++ 스프링 핵심 원리 - 고급편/proxy/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceBasicHandler.java
@@ -0,0 +1,37 @@
+package hello.proxy.config.v2_dynamicproxy.handler;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+@RequiredArgsConstructor
+public class LogTraceBasicHandler implements InvocationHandler {
+    private final Object target;
+    private final LogTrace logTrace;
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        TraceStatus status = null;
+        try {
+            var message = method.getDeclaringClass().getSimpleName() +
+                    "." +
+                    method.getName() +
+                    "()";
+            status = logTrace.begin(message);
+
+            // 로직 호출
+            var result = method.invoke(target, args);
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            if (status != null) {
+                logTrace.exception(status, e);
+            }
+            throw e;
+        }
+    }
+}
