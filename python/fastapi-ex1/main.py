d3f300d265170bed8e82059b4078e9d9df430670
daengdaengLee <gunho1020@gmail.com>
2023-11-30 15:18:48 +0900

    feat(fastapi-tuto): Path Parameters (Part 3)
    - Order matters
    - Cannot redefine a path operation
    - The first one will always be used since the path matches first

--- python/fastapi-ex1/main.py
+++ python/fastapi-ex1/main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
 
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
