bb34c8c592f08983c2d56f078bdf840f233fde71
daengdaengLee <gunho1020@gmail.com>
2023-12-01 12:59:35 +0900

    feat(스프링 핵심 원리 고급): 섹션 6. 스프링이 지원하는 프록시
    - 프록시 팩토리 - 예제 코드1

---
+++ 스프링 핵심 원리 - 고급편/proxy/src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,21 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        var startTime = System.currentTimeMillis();
+
+        var result = invocation.proceed();
+
+        var endTime = System.currentTimeMillis();
+        var resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
