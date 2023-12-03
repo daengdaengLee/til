9f6bc4b51b4596c2e5f7456beea74baea4e5cf69
daengdaengLee <gunho1020@gmail.com>
2023-12-03 12:28:26 +0900

    feat(aoc2023-golang): day1 - puzzle1

---
+++ golang/aoc2023/day1/puzzle1.go
@@ -0,0 +1,63 @@
+package day1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func SolvePuzzle1() {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	path := filepath.Join(dir, "day1", "puzzle1input")
+
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	sum := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), "")
+		l := len(line)
+
+		first := 0
+		for i := 0; i < l; i += 1 {
+			c := line[i]
+			n, err := strconv.Atoi(c)
+			if err == nil {
+				first = n
+				break
+			}
+		}
+
+		last := 0
+		for i := l - 1; i >= 0; i -= 1 {
+			c := line[i]
+			n, err := strconv.Atoi(c)
+			if err == nil {
+				last = n
+				break
+			}
+		}
+
+		n := first*10 + last
+		sum += n
+	}
+
+	fmt.Println("sum=", sum)
+}
---
+++ golang/aoc2023/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "aoc2023/day1"
+
+func main() {
+	day1.SolvePuzzle1()
+}
