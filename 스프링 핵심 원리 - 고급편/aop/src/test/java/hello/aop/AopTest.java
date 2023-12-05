7ea0e98dc459a05c5cc303c7bf2020657cd177c9
daengdaengLee <gunho1020@gmail.com>
2023-12-05 11:24:29 +0900

    feat(스프링 핵심 원리 고급): 섹션 10. 스프링 AOP 구현
    - 스프링 AOP 구현6 - 어드바이스 종류

--- 스프링 핵심 원리 - 고급편/aop/src/test/java/hello/aop/AopTest.java
+++ 스프링 핵심 원리 - 고급편/aop/src/test/java/hello/aop/AopTest.java
@@ -2,7 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -19,8 +19,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 //        AspectV2.class,
 //        AspectV3.class,
 //        AspectV4Pointcut.class,
-        AspectV5Order.LogAspect.class,
-        AspectV5Order.TransactionAspect.class,
+//        AspectV5Order.LogAspect.class,
+//        AspectV5Order.TransactionAspect.class,
+        AspectV6Advice.class,
 })
 public class AopTest {
     @Autowired
