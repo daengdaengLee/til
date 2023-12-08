46c21a68df5ad676c9eafd688b66eabb9f59db03
daengdaengLee <gunho1020@gmail.com>
2023-12-08 12:49:11 +0900

    feat(스프링 핵심 원리 고급): 섹션 13. 스프링 AOP - 실무 주의사항
    - 프록시 기술과 한계 - 스프링의 해결책

--- 스프링 핵심 원리 - 고급편/aop/src/test/java/hello/aop/proxyvs/ProxyDiTest.java
+++ 스프링 핵심 원리 - 고급편/aop/src/test/java/hello/aop/proxyvs/ProxyDiTest.java
@@ -12,7 +12,8 @@ import org.springframework.context.annotation.Import;
 @Slf4j
 @Import({ProxyDiAspect.class})
 //@SpringBootTest(properties = "spring.aop.proxy-target-class=false") // JDK 동적 프록시
-@SpringBootTest(properties = "spring.aop.proxy-target-class=true") // CGLIB 프록시
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=true") // CGLIB 프록시
+@SpringBootTest
 public class ProxyDiTest {
     @Autowired
     MemberService memberService;
