71f93dfa2dc5ffb1b768b1d835d833de096c58a1
daengdaengLee <gunho1020@gmail.com>
2023-12-08 12:13:07 +0900

    feat(스프링 핵심 원리 고급): 섹션 13. 스프링 AOP - 실무 주의사항
    - 프록시와 내부 호출 - 대안2 지연 조회

---
+++ 스프링 핵심 원리 - 고급편/aop/src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,25 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class CallServiceV2 {
+    //    private final ApplicationContext applicationContext;
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+    public void external() {
+        log.info("call external");
+//        var callServiceV2 = applicationContext.getBean(CallServiceV2.class);
+        var callServiceV2 = callServiceProvider.getObject();
+        callServiceV2.internal();
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
