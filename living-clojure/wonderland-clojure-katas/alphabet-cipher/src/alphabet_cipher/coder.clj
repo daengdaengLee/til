b661b3591b2338903a74199244fd52a79ba00d38
daengdaengLee <gunho1020@gmail.com>
2024-01-15 23:37:01 +0900

    feat(living-clojure): training week4 day1

--- living-clojure/wonderland-clojure-katas/alphabet-cipher/src/alphabet_cipher/coder.clj
+++ living-clojure/wonderland-clojure-katas/alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,32 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:require [clojure.string :as string]))
+
+(def row "abcdefghijklmnopqrstuvwxyz")
+
+(def row-count (count row))
+
+(defn index-in-row [token]
+  (loop [tokens (seq row)
+         idx 0]
+    (cond
+      (empty? tokens) -1
+      (= (first tokens) token) idx
+      :else (recur (rest tokens) (+ idx 1)))))
+
+(defn repeat-chars [s]
+  (flatten (repeat (seq s))))
+
+(defn zip-with-keyword [keyword message]
+  (map vector (repeat-chars keyword) (seq message)))
+
+(defn drop-row [token row]
+  (drop (index-in-row token) (repeat-chars row)))
 
 (defn encode [keyword message]
-  "encodeme")
+  (->>
+    (zip-with-keyword keyword message)
+    (map (fn [[a b]] (first (drop-row b (drop-row a (seq row))))))
+    (string/join "")))
 
 (defn decode [keyword message]
   "decodeme")
