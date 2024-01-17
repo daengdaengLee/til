70cd0d49d307aa2290a301c86a87084f0e7d3a41
daengdaengLee <gunho1020@gmail.com>
2024-01-18 00:03:20 +0900

    feat(living-clojure): training week4 day2 - 1

--- living-clojure/wonderland-clojure-katas/alphabet-cipher/src/alphabet_cipher/coder.clj
+++ living-clojure/wonderland-clojure-katas/alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -16,6 +16,11 @@
 (defn index-in-row [token]
   (index-in-tokens token (seq row)))
 
+(defn token-at-index-in-row [index]
+  (if (< index 0)
+    (nth (seq row) (+ index row-count))
+    (nth (seq row) index)))
+
 (defn repeat-chars [s]
   (flatten (repeat (seq s))))
 
@@ -36,11 +41,38 @@
     (zip-with-keyword keyword, message)
     (map (fn [[a b]]
       (let [dropped-row (drop-row a (seq row))
-            index-in-dropped-row (index-in-tokens b dropped-row )
+            index-in-dropped-row (index-in-tokens b dropped-row)
             original-token (first (drop index-in-dropped-row (seq row)))]
         original-token)))
      (string/join "")))
 
 (defn decipher [cipher message]
-  "decypherme")
+  (->>
+    (zip-with-keyword cipher message)
+    (map (fn [[a b]]
+      (let [a-index (index-in-row a)
+            b-index (index-in-row b)
+            a-b (- a-index b-index)]
+        (token-at-index-in-row a-b))))
+    (reduce
+      (fn [acc cur]
+        (if (empty? acc)
+          [[cur]]
+          (let [sub-acc (last acc)
+                start-token (first sub-acc)]
+            (if (= cur start-token)
+              (concat acc [[cur]])
+              (concat (drop-last acc) [(vec (concat sub-acc [cur]))])))))
+      [])
+      (reduce
+        (fn [acc cur]
+          (cond
+            (:done acc) acc
+            (nil? (:first acc)) {:done false, :first cur, :acc [cur]}
+            (= (:first acc) cur) (conj acc {:done true})
+            :else (conj acc {:acc (conj (:acc acc) cur) })))
+        {:done false, :first nil, :acc []})
+      (:acc)
+      (flatten)
+      (string/join "")))
 
