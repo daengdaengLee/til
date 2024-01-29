5429f5fe76a258168a2147f82ceb0ac3a816c5c9
daengdaengLee <gunho1020@gmail.com>
2024-01-29 22:55:07 +0900

    feat(living-clojure): training week4 day3

--- living-clojure/wonderland-clojure-katas/wonderland-number/src/wonderland_number/finder.clj
+++ living-clojure/wonderland-clojure-katas/wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,19 @@
 (ns wonderland-number.finder)
 
+(defn is-wonderland-number [num]
+  (every?
+    (fn [multiplier]
+      (let [a num
+            b (* num multiplier)
+            a-set (set (str a))
+            b-set (set (str b))]
+        (and
+          (not (= a b))
+          (= a-set b-set))))
+    [2 3 4 5 6]))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (loop [num 100000]
+    (if (is-wonderland-number num)
+      num
+      (recur (+ num 1)))))
